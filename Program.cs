ab08b793a25ef3ea189987443a797d97ab0ae2e3
Salvatole1996 <piyapatnakonwan@gmail.com>
2026-02-06 14:40:10 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -44,6 +44,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
     Console.WriteLine($"Checking database connection... {connectionString}");
     try
     {
